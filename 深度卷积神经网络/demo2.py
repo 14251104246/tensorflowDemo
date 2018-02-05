54f25e99bbfc68f44c06bf28148d6a1b265b52b8
14251104246 <523786283@qq.com>
2018-02-05 14:12:28 +0800

    tensorflow可视化

--- 深度卷积神经网络/demo2.py
+++ 深度卷积神经网络/demo2.py
@@ -95,6 +95,7 @@ y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 '''
 训练和评估模型
+dmeo.py有解析
 '''
 cross_entropy = -tf.reduce_sum(y_*tf.log(y_conv))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
@@ -110,7 +111,5 @@ with sess.as_default():
              x:batch[0], y_: batch[1], keep_prob: 1.0})
             print "step %d, training accuracy %g"%(i, train_accuracy)
         train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-
-print "test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
+    print "test accuracy %g"%accuracy.eval(feed_dict={
+        x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
